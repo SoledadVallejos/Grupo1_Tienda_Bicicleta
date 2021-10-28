32c54be3d395d1fe76a7bb5a393022df52ec8fc1
Ariel <Ariel_angel00@outlook.com>
2021-10-28 18:26:36 -0300

    jaa

--- javascript/index.js
+++ javascript/index.js
@@ -1,23 +1,22 @@
 function toggleMenu() {
-    const toggle = document.querySelector('.toggle');
-    const nav = document.querySelector('.nav-ul');
-    toggle.classList.toggle('active');
-    nav.classList.toggle('active');
+  const toggle = document.querySelector(".toggle");
+  const nav = document.querySelector(".nav-ul");
+  toggle.classList.toggle("active");
+  nav.classList.toggle("active");
 }
 
 //saludar dependiendo la hora
 function queHaciendo() {
-    let saludo;
-    let hora = new Date().getHours();
-    if (hora >= 0 && hora < 6) {
-        saludo = "😴 Buenas madrugadas";
-    } else if (hora >= 6 && hora < 12) {
-        saludo = "☕ Buen día!";
-    } else if (hora >= 12 && hora < 19) {
-        saludo = "😎 Buenas tardes!";
-    } else {
-        saludo = "🌙 Buenas noches!";
-    }
-    document.getElementById("demo").innerHTML = saludo;
-
+  let saludo;
+  let hora = new Date().getHours();
+  if (hora >= 0 && hora < 6) {
+    saludo = "😴 Buenas madrugadas";
+  } else if (hora >= 6 && hora < 12) {
+    saludo = "☕ Buen día!";
+  } else if (hora >= 12 && hora < 19) {
+    saludo = "😎 Buenas tardes!";
+  } else {
+    saludo = "🌙 Buenas noches!";
+  }
+  document.getElementById("demo").innerHTML = saludo;
 }
