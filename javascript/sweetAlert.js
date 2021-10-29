3f578e1fcfbcc5d2cc242c0a09e39ce5cd13cd8c
Ariel <ariel_angel00@outlook.com>
2021-10-28 21:24:58 -0300

    Redirección a ofertas personalizadas

--- javascript/sweetAlert.js
+++ javascript/sweetAlert.js
@@ -11,25 +11,27 @@
 
 
 
-  Swal.fire({
-    title: 'quieres acceder a ofertas personalizadas?',
-    showDenyButton: true,
-    showCancelButton: true,
-    confirmButtonText: 'Si',
-    denyButtonText: `No`,
-  }).then((result) => {
-         if (result.isConfirmed) {
-            if(!localStorage.getItem("email")){
-            const { value: email } =  Swal.fire({
-            title: 'Input email address',
-            input: 'email',
-            inputLabel: 'Your email address',
-            inputPlaceholder: 'Ingresa tu email'
-                }).then((object)=>localStorage.setItem("email", object.value))}
-                else Swal.fire("ya se registro con el correo",localStorage.getItem("email"))
-        } else if (result.isDenied) {
-        Swal.fire('Gracias', '', 'info')
+Swal.fire({
+  title: 'quieres acceder a ofertas personalizadas?',
+  showDenyButton: true,
+  showCancelButton: true,
+  confirmButtonText: 'Si',
+  denyButtonText: `No`,
+}).then((result) => {
+  if (result.isConfirmed) {
+    if (!localStorage.getItem("email")) {
+      const { value: email } = Swal.fire({
+        title: 'Input email address',
+        input: 'email',
+        inputLabel: 'Your email address',
+        inputPlaceholder: 'Ingresa tu email'
+      }).then((object) => localStorage.setItem("email", object.value))
     }
-    
-  })
+    else Swal.fire("ya se registro con el correo", localStorage.getItem("email"))
+    window.location.replace("ofertas personalizadas.html")
+  } else if (result.isDenied) {
+    Swal.fire('Gracias', '', 'info')
+  }
+
+})
 console.log(localStorage.getItem("email"))
